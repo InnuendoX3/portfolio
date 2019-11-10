e2de2a858b80c7787fb9f0136703615f9f960a2b
Johan Garzon <johanprograma@hotmail.com>
2019-11-10 22:28:41 +0100

    Add/Remove classes ons navbar links // add/remove sections depending navbar links

--- script/script.js
+++ script/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
    $("#portfolio-sec").hide();
    $("#contact-sec").hide();
+
+   // Navbar - links
+   $("#me-link").click(function(){
+      $("#this-is-me-sec").show();
+      $("#portfolio-sec, #contact-sec").hide();
+      $(this).addClass("active");
+      $("#portfolio-link, #contact-link").removeClass("active");
+
+   });
+
+   $("#portfolio-link").click(function(){
+      $("#portfolio-sec").show();
+      $("#this-is-me-sec, #contact-sec").hide();
+      $(this).addClass("active");
+      $("#me-link, #contact-link").removeClass("active");
+   });
+
+   $("#contact-link").click(function(){
+      $("#contact-sec").show();
+      $("#this-is-me-sec, #portfolio-sec").hide();
+      $(this).addClass("active");
+      $("#me-link, #portfolio-link").removeClass("active");
+   });
+
 });
+
+
 
